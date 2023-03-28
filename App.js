f95348da36c69d7c04129e09b45e9a69559159c3
ALEXZUBIRIA <awexzubiria@gmail.com>
2023-03-27 21:24:02 -0600

    mi primer formulario

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"; 
+
+function App() {
+  return (
+    <div className="container">
+  <h1>Registro</h1>
+  <h1>Nombre</h1>
+  <input type={Text}></input>
+  <h1>Apellido</h1>
+  <input type={Text}></input>
+  <h1>Telefono</h1>
+  <input type={Text}></input>
+ </div>)
+ 
+}
+
+export default App;
