12bacba81177dd0ca90c624d534d70191573e7ca
ALEXZUBIRIA <awexzubiria@gmail.com>
2023-03-27 22:48:56 -0600

    primer cambio

--- App.js
+++ App.js
@@ -3,15 +3,14 @@ import React from "react";
 function App() {
   return (
     <div className="container">
-  <h1>Registro</h1>
-  <h1>Nombre</h1>
+  <h1>Registross0s0s</h1>
+  <h1>Nombresososo</h1>
   <input type={Text}></input>
-  <h1>Apellido</h1>
+  <h1>Apellidosososo</h1>
   <input type={Text}></input>
-  <h1>Telefono</h1>
+  <h1>Telefonososos</h1>
   <input type={Text}></input>
  </div>)
- 
 }
 
 export default App;
